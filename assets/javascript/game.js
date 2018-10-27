be4086691aa4735fcdeabc48194f4952b515e81b
Shuler <msmehaf2@gmail.com>
2018-10-27 09:42:23 -0500

    completed the javascript. still needs styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,78 @@
 $(document).ready(function() {
-
-var box1 = Math.floor(Math.random() * 3) + 1;
-var box2 = Math.floor(Math.random() * 3) + 4;
-var box3 = Math.floor(Math.random() * 3) + 7;
-var box4 = Math.floor(Math.random() * 3) + 10;
-var target = Math.floor(Math.random() * 102) + 19;
-var score = 0;
 var wins = 0;
 var losses = 0;
+var score = 0;
 
 
 function beginGame() {
-$("#targetScore").html(target);
+
+    var box1 = "";
+    var box2 = "";
+    var box3 = "";
+    var box4 = "";
+    var target = "";
+
+    
+
+
+    function resetNumbers() {
+
+        $(targetScore, box1, box2, box3, box4).empty();
+        box1 = Math.floor(Math.random() * 3) + 1;
+        box2 = Math.floor(Math.random() * 3) + 4;
+        box3 = Math.floor(Math.random() * 3) + 7;
+        box4 = Math.floor(Math.random() * 3) + 10;
+        target = Math.floor(Math.random() * 102) + 19;
+        $("#targetScore").html(target);
+    }
+
+    resetNumbers();
+
+   
+   function checkIfOver() {
+    if (score > target) {
+        alert("too High");
+        losses++;
+        $("#losses").text("Losses: " + losses);
+        $("#userScore").empty();
+        score = 0;
+        resetNumbers();
+    } else if (score === target) {
+        alert("You won");
+        wins++;
+        $("#wins").text("Wins: " + wins);
+        $("#userScore").empty();
+        score = 0;
+        resetNumbers();
+    };
 }
+
 
 $("#box1-3").click( function() {
     score += box1;
     $("#userScore").text(score);
-    if (score > target) {
-        alert("too High");
-        $("body").text("OVER");
-        setTimeout(beginGame, 1000);
-    } else if (score === target) {
-        alert("You won");
-        $("body").text("OVER");
-        setTimeout(beginGame, 1000);
-    };
+    checkIfOver();
+
 });
 
 $("#box4-6").click( function() {
     score += box2;
     $("#userScore").text(score);
-    if (score > target) {
-        alert("too High");
-    } else if (score === target) {
-        alert("You won");
-    };
+    checkIfOver();
 });
 
 $("#box7-9").click( function() {
     score += box3;
     $("#userScore").text(score);
-    if (score > target) {
-        alert("too High");
-    } else if (score === target) {
-        alert("You won");
-    };
+    checkIfOver();
 });
 
 $("#box10-12").click( function() {
     score += box4;
     $("#userScore").text(score);
-    if (score > target) {
-        alert("too High");
-    } else if (score === target) {
-        alert("You won");
-    };
+    checkIfOver();
 });
-
+}
 
 beginGame();
 
